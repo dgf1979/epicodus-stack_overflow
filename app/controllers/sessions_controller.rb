5470cd318001b739abd64ad0f8a37918d141f3db
Ian MacDonald & Drew Finstrom <ian@iancmacdonald.com>
2015-07-06 10:30:56 -0700

    Update form styling, add log-out button

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,15 +5,18 @@ class SessionsController < ApplicationController
   def create
     @user = User.authenticate(params[:email], params[:password])
     if @user
+      flash[:notice] = "You have logged in, #{@user.name}"
       session[:user_id] = @user.id
       redirect_to user_path(@user)
     else
+      flash[:alert] = "Something went wrong, please try again"
       redirect_to log_in_path
     end
 
   end
 
   def destroy
+    flash[:alert] = "You have logged out"
     session[:user_id] = nil
     redirect_to users_path
   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,7 +24,8 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      redirect_to @user, notice: 'User was successfully created.'
+      flash[:notice] = 'User was successfully created.'
+      redirect_to @user
     else
       render :new
     end
@@ -33,16 +34,18 @@ class UsersController < ApplicationController
   # PATCH/PUT /users/1
   def update
     if @user.update(user_params)
-      redirect_to @user, notice: 'User was successfully updated.'
+      flash[:notice] = 'User was successfully updated.'
+      redirect_to @user
     else
       render :edit
     end
   end
 
   # DELETE /users/1
-  def destroy
+    def destroy
     @user.destroy
-    redirect_to users_url, notice: 'User was successfully destroyed.'
+    flash[:notice] = 'User was successfully destroyed.'
+    redirect_to users_url
   end
 
   private
@@ -53,6 +56,6 @@ class UsersController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def user_params
-      params.require(:user).permit(:name, :email, :password, :admin)
+      params.require(:user).permit(:name, :email, :password, :password_confirmation, :admin)
     end
 end
