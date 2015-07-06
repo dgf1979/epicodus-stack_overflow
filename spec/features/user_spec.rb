58bd9bbec5a76eb62b4a3c4b4b68a3a1ed559223
Ian MacDonald & Drew Finstrom <ian@iancmacdonald.com>
2015-07-06 11:26:13 -0700

    validate user integration coverage

---
+++ spec/features/user_spec.rb
@@ -0,0 +1,56 @@
+require 'rails_helper'
+
+describe "user management" do
+  it "signs up a new user" do
+    visit new_user_path
+    fill_in "Name", with: "John"
+    fill_in "Email", with: "john@place.com"
+    fill_in "Password", with: "password"
+    fill_in "Password confirmation", with: "password"
+    click_on 'Create User'
+    expect(page).to have_content 'John'
+  end
+
+  it "fails to sign up a new user" do
+    visit new_user_path
+    fill_in "Name", with: ""
+    fill_in "Email", with: "john@place.com"
+    fill_in "Password", with: "password"
+    fill_in "Password confirmation", with: "password"
+    click_on 'Create User'
+    expect(page).to have_content 'error'
+  end
+
+  it "modifies a user" do
+    user = FactoryGirl.create(:user)
+    visit edit_user_path(user)
+    fill_in "Name", with: "Ruby"
+    click_on 'Update User'
+    expect(page).to have_content 'Ruby'
+  end
+
+  it "fails to modify a user" do
+    user = FactoryGirl.create(:user)
+    visit edit_user_path(user)
+    fill_in "Name", with: ""
+    click_on 'Update User'
+    expect(page).to have_content 'error'
+  end
+
+  it "destroys a user" do
+    user = FactoryGirl.create(:user)
+    visit users_path
+    click_on "Destroy"
+    expect(page).to_not have_content 'Josh'
+  end
+end
+
+
+
+#
+# describe "authentication" do
+#   it "adds a user" do
+#
+#   end
+#
+# end
