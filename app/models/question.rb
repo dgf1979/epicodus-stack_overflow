044f108bbe591d498a5f54efe9a04be2899332b8
Drew & Ian <student@epicodus.com>
2015-07-07 12:34:07 -0700

    add admin questions-per-day

--- app/models/question.rb
+++ app/models/question.rb
@@ -11,9 +11,15 @@ class Question < ActiveRecord::Base
     where("created_at < ? AND created_at > ?", Time.new.utc, start_time)
   }
 
+  def self.by_day
+    query = "select date(created_at) as day, count(*) from questions group by day;"
+    return Question.connection.exec_query(query)
+  end
+
   def send_new_question_email
     if self.user_id
       UserMailer.new_question_email(User.find(self.user_id), self).deliver_now
     end
   end
+
 end
